89f938143bde31779c8dcfb02a58b03620576bc7
unknown <YY408@ccs.local>
2025-02-07 20:53:14 +0800

    Initial commit with index file with POST

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require['express'];
+const express = require('express');
 const app = express();
 const port = 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
 
 //Define a route for the home page
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
+});
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Reseived: ${JSON.stringify(data)}`);
 });
